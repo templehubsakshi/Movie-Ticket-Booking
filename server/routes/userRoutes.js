c2b4e113afc7f4028c5bb8e0b3bc685354d839bc
Sakshi Gupta <155846503+templehubsakshi@users.noreply.github.com>
2026-04-06 19:58:59 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,21 +1,12 @@
 import express from "express";
-import {
-  getFavorites,
-  getUserBookings,
-  updateFavorite,
-} from "../controllers/userController.js";
+import { getFavorites, getUserBookings, updateFavorite } from "../controllers/userController.js";
+import { protectRoute } from "../middleware/auth.js";
 
 const userRouter = express.Router();
 
-// logged-in user apni bookings dekh sakta hai
-// yaha userId req.auth() se aata hai
-userRouter.get("/bookings", getUserBookings);
-
-// user kisi movie ko favorite / unfavorite karta hai
-// movieId req.body se aata hai
-userRouter.post("/update-favorite", updateFavorite);
-
-// user apni favorite movies ki list dekh sakta hai
-userRouter.get("/favorites", getFavorites);
+// All user routes require a valid JWT
+userRouter.get("/bookings", protectRoute, getUserBookings);
+userRouter.post("/update-favorite", protectRoute, updateFavorite);
+userRouter.get("/favorites", protectRoute, getFavorites);
 
 export default userRouter;
