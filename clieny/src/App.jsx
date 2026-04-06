a60b5bc97c4c73164b27d6ad00c10f6b64e11ea7
Sakshi Gupta <155846503+templehubsakshi@users.noreply.github.com>
2026-04-06 20:03:14 +0530

    Update App.jsx

--- clieny/src/App.jsx
+++ clieny/src/App.jsx
@@ -1,60 +1,57 @@
+import { Route, Routes, useLocation } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 import Navbar from "./components/Navbar";
-import { Route, Routes, useLocation } from "react-router-dom";
+import Footer from "./components/Footer";
+import { ProtectedRoute, AdminRoute } from "./components/ProtectedRoute";
+
 import Home from "./pages/Home";
 import Movies from "./pages/Movies";
 import MovieDetails from "./pages/MovieDetails";
 import SeatLayout from "./pages/SeatLayout";
 import MyBookings from "./pages/MyBookings";
 import Favorite from "./pages/Favorite";
-import { Toaster } from "react-hot-toast";
-import Footer from "./components/Footer";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import PaymentLoading from "./pages/PaymentLoading";
+
 import Layout from "./pages/admin/Layout";
 import Dashboard from "./pages/admin/Dashboard";
 import AddShows from "./pages/admin/AddShows";
 import ListShows from "./pages/admin/ListShows";
 import ListBookings from "./pages/admin/ListBookings";
-import { useAppContext } from "./context/AppContext";
-import { SignIn } from "@clerk/clerk-react";
-import Loading from "./components/Loading";
 
 const App = () => {
   const isAdminRoute = useLocation().pathname.startsWith("/admin");
 
-  const { user } = useAppContext();
-
   return (
     <>
-      <Toaster />
+      <Toaster position="top-center" />
       {!isAdminRoute && <Navbar />}
+
       <Routes>
+        {/* Public routes */}
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
         <Route path="/movies/:id" element={<MovieDetails />} />
-        <Route path="/movies/:id/:date" element={<SeatLayout />} />
-        <Route path="/my-bookings" element={<MyBookings />} />
-        <Route path="/loading/:nextUrl" element={<Loading />} />
-        <Route path="/favorite" element={<Favorite />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
 
-        {/* Admin Routes */}
-        <Route
-          path="/admin/*"
-          element={
-            user ? (
-              <Layout />
-            ) : (
-              <div className="min-h-screen flex justify-center items-center">
-                <SignIn fallbackRedirectUrl={"/admin"} />
-              </div>
-            )
-          }
-        >
+        {/* Auth-required routes */}
+        <Route path="/movies/:id/:date" element={<ProtectedRoute><SeatLayout /></ProtectedRoute>} />
+        <Route path="/my-bookings" element={<ProtectedRoute><MyBookings /></ProtectedRoute>} />
+        <Route path="/favorite" element={<ProtectedRoute><Favorite /></ProtectedRoute>} />
+        <Route path="/loading/:nextUrl" element={<ProtectedRoute><PaymentLoading /></ProtectedRoute>} />
+
+        {/* Admin-only routes */}
+        <Route path="/admin/*" element={<AdminRoute><Layout /></AdminRoute>}>
           <Route index element={<Dashboard />} />
           <Route path="add-shows" element={<AddShows />} />
           <Route path="list-shows" element={<ListShows />} />
           <Route path="list-bookings" element={<ListBookings />} />
         </Route>
       </Routes>
+
       {!isAdminRoute && <Footer />}
     </>
   );
